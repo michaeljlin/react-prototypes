fcac7c2d9aa35df9c33529703fb83e4339c9b752
michaeljlin <mjameslin@gmail.com>
2017-10-16 20:15:53 -0700

    React-intro done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    return Math.floor(Math.random()*1000 +1);
+}
+
+function greeting(userObject){
+    // var divElement = document.createElement("div");
+    // divElement.classList.add("container");
+    //
+    // var headerElement1 = document.createElement("h1");
+    // headerElement1.textContent = "Welcome " + userObject.name;
+    //
+    // var headerElement2 = document.createElement("h2");
+    // headerElement2.classList.add("text-muted");
+    // headerElement2.textContent = userObject.luckyNumber;
+    //
+    // divElement.appendChild(headerElement1);
+    // divElement.appendChild(headerElement2);
+    //
+    // return divElement;
+
+
+    return <div className='container'>
+        <h1>Welcome {userObject.name}</h1>
+        <h2 className='text-muted'>{userObject.luckyNumber}</h2>
+    </div>
+}
+
+var user = {
+    name: 'Michael',
+    luckyNumber: luckyNumber()
+};
+
+var test = greeting(user);
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
