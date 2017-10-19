2693672ee861cd121629182e249485d9922305a8
michaeljlin <mjameslin@gmail.com>
2017-10-19 12:56:16 -0700

    Feature set 2

---
+++ src/components/stopwatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Stopwatch extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This will be the stopwatch</h3>
+            </div>
+        );
+    }
+}
+
+export default Stopwatch;
