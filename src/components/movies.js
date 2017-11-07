159983ef6262a2f1c174c0b84a506a2701befa9c
michaeljlin <mjameslin@gmail.com>
2017-11-07 14:52:42 -0800

    Additional adjustments

--- src/components/movies.js
+++ src/components/movies.js
@@ -4,6 +4,19 @@ export default (props)=>{
     console.log(props.info);
     const { info } = props;
 
+    // console.log(info.hasOwnProperty('rights'));
+
+    let rights = "Copyright Unknown";
+    let rental = "Not Available";
+
+    if(info.hasOwnProperty('rights')){
+        rights = info.rights.label;
+    }
+
+    if(info.hasOwnProperty('im:rentalPrice')){
+        rental = info['im:rentalPrice'].label;
+    }
+
     const boxStyle = {
         'margin-bottom': '5px'
     };
@@ -13,8 +26,11 @@ export default (props)=>{
             <img src={info['im:image'][2].label}/>
             <h3>{info['im:name'].label}</h3>
             <p>{info['summary'].label}</p>
+            <p>{rights}</p>
             <p>Released: {info['im:releaseDate'].attributes.label}</p>
-            <a className="btn btn-outline-primary" href={info['id'].label}>Buy on iTunes</a>
+            <p>Buy: {info['im:price'].label}</p>
+            <p>Rent: {rental}</p>
+            <a className="btn btn-outline-primary" href={info['id'].label}>Watch on iTunes</a>
 
         </div>
     )
