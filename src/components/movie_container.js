7387d2637d28a1c2c9bf9d57595a7fab7d79235f
michaeljlin <mjameslin@gmail.com>
2017-11-07 12:29:11 -0800

    Feature set 2

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import Movie from './movies';
+import Axios from 'axios';
 
 class MoviesContainer extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            movies: []
+        };
+    }
+
+    componentWillMount(){
+        const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+
+        Axios.get(url)
+            .then( (resp)=>{
+                console.log("Resp: ",resp);
+                this.setState({
+                    movies: resp.data.feed.entry
+                });
+            })
+            .catch( (error) =>{
+
+            });
+    }
+
     render(){
+        console.log("State is: ",this.state.movies);
         return(
             <div>
                 <h2>Movie Container</h2>
